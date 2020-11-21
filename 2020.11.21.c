b33b7723e5e7929ba540e246611115b89491bbba
Elvis2000717 <1147445979@qq.com>
2020-11-21 22:35:10 +0800

    2020.11.21

---
+++ 2020.11.21.c
@@ -0,0 +1,31 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//
+//int main()
+//{
+//	//按f9打断点  直接按f5运行到断点处
+//	/*int a = 1;
+//	a = 2;
+//	a = 3;
+//
+//	int b = 4660;
+//	char ch = 'a';
+//	char str1[] = { 'a', 'b', 'c' };
+//	char str2[] = "abcd1";
+//	char str3[] = "陕西";
+//	str3[3] += 1;
+//
+//	printf("%s\n", str1);
+//	printf("%s\n", str2);*/
+//
+//	int a, b;
+//	scanf("%d %d", &a, &b);
+//	if (a > b)
+//	{
+//		printf("%d", a);
+//	}
+//	else {
+//		printf("%d", b);
+//	}
+//	return 0;
+//}
