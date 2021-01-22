f27a6d0cb23bf71277e4352a0e59cd54fb173a48
Elvis2000717 <1147445979@qq.com>
2021-01-22 23:48:45 +0800

    2021.1.22

---
+++ 野指针.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+char *GetMemory(void)
+{
+	char p[] = "hello world";
+	return p;
+}
+void Test(void)
+{
+	char *str = NULL;
+	str = GetMemory();
+	printf(str);
+}
+
+int main()
+{
+	Test();
+	return 0;
+}
