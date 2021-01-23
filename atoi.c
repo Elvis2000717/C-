2c1529051a30e40070792eedf886ff68a312fbb7
Elvis2000717 <1147445979@qq.com>
2021-01-23 20:21:11 +0800

    2021.1.23

---
+++ atoi.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int atoi(const char* str)
+{
+	int s = 0;
+	bool flag = false;
+
+	while (*str == ' ')
+	{
+		str++;
+	}
+
+	if (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+		{
+			flag = true;
+		}
+		str++;
+	}
+	else if (*str < '0' || *str > '9')
+	{
+		s = 216483548;
+		return s;
+	}
+
+	while (*str != '\0' && *str >= '0' && *str <= '9')
+	{
+		s = s * 10 + (*str - '0');
+		str++;
+	}
+
+	return s;
+}
+
+int main()
+{
+	char a[] = { "12345" };
+	int b = atoi(a);
+	printf("%s\n", a);
+	printf("%d\n", b);
+	return 0;
+}
